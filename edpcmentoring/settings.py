12e5c69f651b9665b2a65d9bcd00078c51c1414d
Rich Wareham <rich@richwareham.com>
2016-04-24 22:04:50 +0100

    rename mentoringmatching->matching

--- edpcmentoring/settings.py
+++ edpcmentoring/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
     'mentoring',
     'mentoringmeetings',
-    'mentoringmatching',
+    'matching',
     'training',
 ]
 
--- mentoringmatching/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class MentoringMatchingConfig(AppConfig):
-    name = 'mentoringmatching'
-    verbose_name = 'Mentoring Relationship Matching'
