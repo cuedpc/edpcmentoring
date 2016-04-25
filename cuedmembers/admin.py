ad40a69c5736a273179d30541114313707151888
Rich Wareham <rich@richwareham.com>
2016-04-25 23:55:37 +0100

    improve cuedmembers adminsite

--- cuedmembers/admin.py
+++ cuedmembers/admin.py
@@ -8,6 +8,13 @@ class MemberAdmin(admin.ModelAdmin):
 
     list_filter = ('is_active', 'arrived_on')
 
+    def get_queryset(self, request):
+        return Member.objects.order_by('user__username')
+
+    def crsid(self, obj):
+        return obj.user.username
+    crsid.admin_order_field = 'user__username'
+
     def full_name(self, obj):
         return obj.user.get_full_name()
 
@@ -15,12 +22,27 @@ class MemberAdmin(admin.ModelAdmin):
         if obj.research_group is None:
             return None
         return obj.research_group.division
+    division.admin_order_field = 'research_group__division__letter'
+
+    def research_group(self, obj):
+        return obj.research_group
+    research_group.admin_order_field = 'research_group__name'
 
 admin.site.register(Member, MemberAdmin)
 
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('member', 'status', 'start_on', 'end_on')
-    list_filter = ('status',)
+    list_display = ('crsid', 'full_name', 'status', 'start_on', 'end_on')
+    list_filter = ('status', 'start_on', 'end_on')
+
+    def get_queryset(self, request):
+        return Status.objects.order_by('member__user__username')
+
+    def crsid(self, obj):
+        return obj.member.user.username
+    crsid.admin_order_field = 'member__user__username'
+
+    def full_name(self, obj):
+        return obj.member.user.get_full_name()
 
 admin.site.register(Status, StatusAdmin)
 
