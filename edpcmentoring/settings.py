66877175d5052e58fd30c3450877ed8a9d6bf6a7
Rich Wareham <rich@richwareham.com>
2016-04-25 09:25:31 +0100

    split project light templates into standalone app

--- edpcmentoring/settings.py
+++ edpcmentoring/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'ucamwebauth',
     'cuedmembers',
+    'projectlight',
 
     'mentoring',
     'matching',
---
+++ projectlight/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProjectlightConfig(AppConfig):
+    name = 'projectlight'
