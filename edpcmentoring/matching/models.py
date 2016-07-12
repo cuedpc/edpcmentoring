a9bd89f9c87c7fc7d9f08fe0ea8ec1dc6f11c0ef
Rich Wareham <rich@richwareham.com>
2016-07-12 11:18:03 +0100

    matching: update Invitation model to match design
    The Invitation model was OK as it was but rename some fields and expand the
    documentation.

--- edpcmentoring/matching/models.py
+++ edpcmentoring/matching/models.py
@@ -24,10 +24,38 @@ class Invitation(models.Model):
     """
     An invitation to form a mentoring relationship.
 
+    Invites are the mechanism where mentor/mentee relationships are created. An
+    invite can be created by anyone but, in that case, they need to be the
+    mentor or mentee of the invite. Users with the "matchmake" permission can
+    invite any two users to form a mentor/mentee relationship.
+
+    Each invitation records who the mentor and mentee are to be and the user who
+    created the invite. (This is useful to determine which of the mentors and
+    mentees should actually be notified.) The creation date is also stored to
+    allow for some form of automatic expiry.
+
+    An "active" invite is one which is not expired and is still awaiting a
+    response from the mentor or mentee. An invitation which is declined by
+    either mentor or mentee is marked as inactive even if the other party has
+    not responded. The :py:attr:`.deactivated_on` date records when this invite
+    became inactive either through being declined by one party or accepted by
+    both.
+
+    Should the invite result in a new relationship, this is recorded in the
+    :py:attr:`.created_relationship` field.
+
+    .. attribute:: mentor
+
+        The user who is to be the mentor.
+
+    .. attribute:: mentee
+
+        The user who is to be the mentee.
+
     """
     class Meta(object):
         permissions = (
-            ('matchmake', 'Can matchmake mentors and mentees'),
+            ('matchmake', 'Can matchmake mentors and mentees with each other'),
         )
 
     # The possible responses to an invitation.
@@ -48,10 +76,8 @@ class Invitation(models.Model):
     mentee_response = models.CharField(max_length=1, choices=RESPONSES)
 
     is_active = models.BooleanField(default=True)
+    deactivated_on = models.DateField(blank=True, null=True)
 
-    # Note: English spelling, not American
-    cancelled_on = models.DateField(blank=True, null=True)
-
-    relationship_started = models.ForeignKey(
+    created_relationship = models.ForeignKey(
         Relationship, blank=True, null=True,
         related_name='started_by_invitations')
