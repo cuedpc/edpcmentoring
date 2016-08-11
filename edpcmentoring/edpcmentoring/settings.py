2d04d5b8a01c23b004622100460d41a758d8ef40
StephenS <Stephen.shorrock@gmail.com>
2016-08-11 16:09:12 +0100

    Update settings.py

--- edpcmentoring/edpcmentoring/settings.py
+++ edpcmentoring/edpcmentoring/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
