f62125cd166a6367663985206d06f38716934878
StephenS <Stephen.shorrock@gmail.com>
2016-10-13 10:57:26 +0100

    Remove reference to test database
    MWS3 (mysql version) does not come with a test database

--- deploy/setup_mws3.py
+++ deploy/setup_mws3.py
@@ -63,7 +63,7 @@ username="%s_web" % dbname
 
 
 try:
-    con = mdb.connect('localhost', 'root', rootpass, 'test');
+    con = mdb.connect('localhost', 'root', rootpass, '');
     cur = con.cursor()
 
     cur.execute("create database %s " % dbname)
