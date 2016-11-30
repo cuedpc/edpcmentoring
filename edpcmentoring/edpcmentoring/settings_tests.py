a005ee2dfc1803da7e8c2f6ac1a72e93e2e94999
S-Stephen <stephen.shorrock@gmail.com>
2016-11-30 19:42:07 +0000

    added values to test env inc changes to allow native django auth

--- edpcmentoring/edpcmentoring/settings_tests.py
+++ edpcmentoring/edpcmentoring/settings_tests.py
@@ -6,3 +6,19 @@ This configuration builds on the configuration in settings_development.
 """
 # pylint: disable=wildcard-import,unused-wildcard-import
 from .settings_development import *
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+SECRET_KEY='orefjkdv'
+DEBUG = True
+
+AUTHENTICATION_BACKENDS = [
+    # 'ucamwebauth.backends.RavenAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+# A faster hash for test authentication
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
