cffb759e0f775a33d0d298adb9ecf4735c9c1f46
StephenS <S-Stephen@users.noreply.github.com>
2016-11-30 20:03:01 +0000

    tabs

--- edpcmentoring/rest/tests.py
+++ edpcmentoring/rest/tests.py
@@ -14,8 +14,8 @@ from django.core.management import call_command
 
 # A list holding the fixtures to load
 TEST_FIXTURES = [
-    'cuedmembers/test_users_and_members',
-    'mentoring/test_relationships',
+	'cuedmembers/test_users_and_members',
+	'mentoring/test_relationships',
 ]
 
 #def run():
@@ -34,7 +34,7 @@ class CheckDenyAccessCase(TestCase):
 	#def test_login(self):
 	def setUp(self):
 		call_command('loaddata', *TEST_FIXTURES)
-		setup_test_environment()
+	        setup_test_environment()
 		users=[]
 		users.append(User.objects.get(username='test0001'))
 		users.append(User.objects.get(username='test0002'))
@@ -77,13 +77,13 @@ class CheckDenyAccessCase(TestCase):
 
 	def tesit_api_current(self):
 		
-                # log in user:
-                response = self.client.login(username='test0001', password='test')
-                self.assertEqual(response,True)
+		# log in user:
+		response = self.client.login(username='test0001', password='test')
+		self.assertEqual(response,True)
 
-                # attempt to view user/2 should be denied
-                res = self.client.get('/api/current',follow=True)
-                self.assertEqual(res.status_code, 200)
+		# attempt to view user/2 should be denied	
+		res = self.client.get('/api/current',follow=True)
+		self.assertEqual(res.status_code, 200)
 		myj = json.loads(res.content)
 		self.assertEqual(myj[0][u'username'],'test0001',"logged in as user test0001")
 
@@ -96,10 +96,10 @@ class CheckDenyAccessCase(TestCase):
 
                 # attempt to view user/2 should be denied
                 res = self.client.get('/api/current_preferences',follow=True)
-		self.assertEqual(res.status_code, 200)
+                self.assertEqual(res.status_code, 200)
 
 
-	def tesit_api_proxy(self):
+        def tesit_api_proxy(self):
 		
 		#deny - not sure what this is used for atm
 		#     - implement as part of management interface
@@ -113,7 +113,7 @@ class CheckDenyAccessCase(TestCase):
 
         def tesit_api_groups(self):
 		#deny - again to implement as part of the manager interface
-		#     - when implemented make sure only managers can view
+	        #     - when implemented make sure only managers can view
 		
                 response = self.client.login(username='test0001', password='test')
                 self.assertEqual(response,True)
@@ -127,27 +127,27 @@ class CheckDenyAccessCase(TestCase):
 		
 
 		# create a relationship test0010 - test0011, mentee - mentor
-		rel = Relationship.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),is_active=True)
+                rel = Relationship.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),is_active=True)
 		# what is the id of the relationship we have?
-		print rel.id
+                print rel.id
 
-		#TODO use regular user (test0005 hopefully!) to test that they can not access the relatiomship url:
-	        # log in user:
+	        #TODO use regular user (test0005 hopefully!) to test that they can not access the relatiomship 
+                # log in user:
                 response = self.client.login(username='test0005', password='test')
                 self.assertEqual(response,True)
 
                 # attempt to view user/2 should be denied
                 res = self.client.get('/api/basicrel/'+str(rel.id),follow=True)
-		self.assertEqual(res.status_code, 403, "deny access for non mentor mentee (and manager - TODO)")
+                self.assertEqual(res.status_code, 403, "deny access for non mentor mentee (and manager - TODO)")
 
-		# check that both 10 and 11 can view it (TODO check manager)
+                # check that both 10 and 11 can view it (TODO check manager)
 		
                 response = self.client.login(username='test0010', password='test')
                 self.assertEqual(response,True,"test0010 logged in")
 
                 # attempt to view relationshio should be accepted
                 res = self.client.get('/api/basicrel/'+str(rel.id),follow=True)
-		self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
+                self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
 
 			
                 response = self.client.login(username='test0011', password='test')
@@ -155,7 +155,7 @@ class CheckDenyAccessCase(TestCase):
 
                 # attempt to view relationshio should be accepted
                 res = self.client.get('/api/basicrel/'+str(rel.id),follow=True)
-		self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
+                self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
 
 	        #super user		
                 response = self.client.login(username='test0001', password='test')
@@ -163,38 +163,38 @@ class CheckDenyAccessCase(TestCase):
 
                 # attempt to view relationshio should be accepted
                 res = self.client.get('/api/basicrel/'+str(rel.id),follow=True)
-		self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
+                self.assertEqual(res.status_code, 200, "deny access for non mentor mentee (and manager - TODO)")
 
 
 	def test_api_meetings(self):
-		# only allow mentor, mentee and manager ability to post a new meeting
+                # only allow mentor, mentee and manager ability to post a new meeting
 
                 response = self.client.login(username='test0007', password='test')
                 self.assertEqual(response,True,"test0007 logged in")
 
-		rel = Relationship.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),is_active=True)
+                rel = Relationship.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),is_active=True)
 	
                 res = self.client.post('/api/meetings/',{'approximate_duration':40,'held_on':now().date(),'relationship':'/api/basicrel/'+str(rel.id)+'/'})
                 # 400 invalid data (post)
-		self.assertEqual(res.status_code, 400, "deny creation of meeting where user not superuser and not mentor/mentee")
+                self.assertEqual(res.status_code, 400, "deny creation of meeting where user not superuser and not mentor/mentee")
 		
 
-		# A super user should be able to register a meeting
+                # A super user should be able to register a meeting
                 response = self.client.login(username='test0001', password='test')
                 self.assertEqual(response,True,"test0001 (superuser) logged in")
 
                 res = self.client.post('/api/meetings/',{'approximate_duration':40,'held_on':now().date(),'relationship':'/api/basicrel/'+str(rel.id)+'/'})
-		# 201 -> created
-		self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
+                # 201 -> created
+                self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
 
 		
-		# The Mentor should be able to register a meeting
+                # The Mentor should be able to register a meeting
                 response = self.client.login(username='test0010', password='test')
                 self.assertEqual(response,True,"test0010 (mentor) logged in")
 
                 res = self.client.post('/api/meetings/',{'approximate_duration':40,'held_on':now().date(),'relationship':'/api/basicrel/'+str(rel.id)+'/'})
-		# 201 -> created
-		self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
+                # 201 -> created
+                self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
 
 
 		# The Mentee should be able to register a meeting
@@ -202,8 +202,8 @@ class CheckDenyAccessCase(TestCase):
                 self.assertEqual(response,True,"test0011 (mentor) logged in")
 
                 res = self.client.post('/api/meetings/',{'approximate_duration':40,'held_on':now().date(),'relationship':'/api/basicrel/'+str(rel.id)+'/'})
-		# 201 -> created
-		self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
+                # 201 -> created
+                self.assertEqual(res.status_code, 201, "allow creation of meeting where user is superuser and not mentor/mentee")
 
 
 	def test_api_invitations(self):
@@ -213,14 +213,14 @@ class CheckDenyAccessCase(TestCase):
 
 		# only allow mentor, mentee and manager ability to post a new meeting
 
-		inv = Invitation.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),created_by=User.objects.get(username='test0011'))
+	        inv = Invitation.objects.create(mentor=User.objects.get(username='test0010'),mentee=User.objects.get(username='test0011'),created_by=User.objects.get(username='test0011'))
 
                 response = self.client.login(username='test0007', password='test')
                 self.assertEqual(response,True,"test0007 logged in")
 
                 res = self.client.put('/api/invitations/'+str(inv.id)+'/',{'mentor_response':'"A"'},'application/json')
                 # 400 invalid data (post)
-		self.assertEqual(res.status_code, 400, "deny update of invitation where user not superuser and not mentor/mentee")
+                self.assertEqual(res.status_code, 400, "deny update of invitation where user not superuser and not mentor/mentee")
 	
                 response = self.client.login(username='test0001', password='test')
                 self.assertEqual(response,True,"test0001 (superuser) logged in")
@@ -236,18 +236,18 @@ class CheckDenyAccessCase(TestCase):
 
                 res = self.client.put('/api/invitations/'+str(inv.id)+'/',json.dumps({'mentor_response':'A'}),'application/json')
                 # 200 updated 
-		self.assertEqual(res.status_code, 200, "allow update of invitation where user mentor updating mentor_response")
+                self.assertEqual(res.status_code, 200, "allow update of invitation where user mentor updating mentor_response")
 	
                 response = self.client.login(username='test0011', password='test')
                 self.assertEqual(response,True,"test0011 (mentee) logged in")
 
                 res = self.client.put('/api/invitations/'+str(inv.id)+'/',json.dumps({'mentor_response':'A'}),'application/json')
                 # 400 updated 
-		self.assertEqual(res.status_code, 400, "deny update of invitation where mentee updating mentor acceptance")
+                self.assertEqual(res.status_code, 400, "deny update of invitation where mentee updating mentor acceptance")
 	
                 res = self.client.put('/api/invitations/'+str(inv.id)+'/',json.dumps({'mentee_response':'A'}),'application/json')
                 # 200 updated 
-		self.assertEqual(res.status_code, 200, "allow update of invitation where mentee updating mentee acceptance")
+                self.assertEqual(res.status_code, 200, "allow update of invitation where mentee updating mentee acceptance")
 
 
 	
@@ -255,11 +255,10 @@ class CheckDenyAccessCase(TestCase):
 		'''
 		My invitations accessible by get only
 		'''
-				
                 response = self.client.login(username='test0010', password='test')
                 self.assertEqual(response,True,"test0010 (mentor) logged in")
 
 		#MyInvitations are read only
                 res = self.client.options('/api/myinvitations/')
-		self.assertEqual(str(res['Allow']),"GET, OPTIONS")
+                self.assertEqual(str(res['Allow']),"GET, OPTIONS")
 
