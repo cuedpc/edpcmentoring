6b3c6b58e6d66778c6c5320bf810959cde487111
S-Stephen <stephen.shorrock@gmail.com>
2016-12-12 11:03:51 +0000

    added end relationship handler (though v.basic logic)

--- edpcmentoring/autonag/signalhandlers.py
+++ edpcmentoring/autonag/signalhandlers.py
@@ -8,11 +8,23 @@ from mentoring.models import Relationship
 @receiver(post_save, sender=Relationship, dispatch_uid='relationship_create')
 def relationship_post_save_handler(created, instance, **_):
     # Only notify if this is a new *active* relationship
-    if not created or not instance.is_active:
+    if not created: # or not instance.is_active:
+        if instance.ended_on:
+        # Send these if the relationship has ended
+            send([instance.mentor], 'end_mentee', {'relationship': instance})
+            send([instance.mentee], 'end_mentor', {'relationship': instance})
+            
         return
+
 # These will be sent via a cron on the autonag 
-    send([instance.mentor], 'new_mentee', {'relationship': instance})
-    send([instance.mentee], 'new_mentor', {'relationship': instance})
+    if instance.is_active:
+        send([instance.mentor], 'new_mentee', {'relationship': instance})
+        send([instance.mentee], 'new_mentor', {'relationship': instance})
+        return
+
+## Send these if the relationship has ended
+#    send([instance.mentor], 'end_mentee', {'relationship': instance})
+#    send([instance.mentee], 'end_mentee', {'relationship': instance})
 
 def create_notice_types(**_):
     if 'pinax.notifications' in settings.INSTALLED_APPS:
@@ -22,6 +34,10 @@ def create_notice_types(**_):
             'new_mentor', 'New mentor', 'you have a new mentor')
         NoticeType.create(
             'new_mentee', 'New mentee', 'you have a new mentee')
+        NoticeType.create(
+            'end_mentee', 'End mentee', 'A mentee relatiohip has ended')
+        NoticeType.create(
+            'end_mentor', 'End mentor', 'A mentor relatiohip has ended')
     else:
         print('Skipping creation of NoticeTypes as notification app not found')
 
